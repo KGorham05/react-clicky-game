8a5c338f19fcad97f965539b35abfe826ddd8a4d
Kgor <kevin.gorham@gmail.com>
2019-04-19 22:28:15 -0700

    created heroes.json file

---
+++ src/components/Nav/index.js
@@ -0,0 +1,10 @@
+import React from "React";
+import "./style.css";
+
+function Nav {
+    return (
+        <div className="n-bar">
+            <p className="">Clicky Game</p>
+        </div>
+    )
+}
