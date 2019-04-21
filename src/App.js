adb5359cfa7c3a62bc5f0f595a98054bd2dc6bf3
Kgor <kevin.gorham@gmail.com>
2019-04-20 19:45:24 -0700

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import React, { Component } from 'react';
-import './App.css';
+import heroes from "../heroes.json"
+
 
 class App extends Component {
+
+  state = {
+    heroes,
+    score: 0,
+    highScore: 0
+  }
+
+
+  shuffle = array => {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
+  // function for when a card is clicked on
+  clickCounter = id => {
+
+  }
+
+  gameOver = () => {
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      // Here is where I render component units
+      <Wrapper>
+        <Nav score={this.state.score} highscore={this.state.highScore} />
+        <GameBoard>
+          {/* loop through and display the cards */}
+          {this.state.heroes.map(hero => {
+            <Card
+              clickCounter={this.clickCounter}
+              id={hero.id}
+              key={hero.name}
+              image={hero.image}
+            />
+          })}
+        </GameBoard>
+        <Footer />
+      </Wrapper>
+
+
+
     );
   }
 }
